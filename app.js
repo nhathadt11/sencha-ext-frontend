d0eec3bbf8747aee924ea753d38c59792d2ea285
Nhat Ha <nhathadt11@gmail.com>
2018-03-11 11:52:01 +0700

    Revert "Fix book cannot update due to invalid use of method"
    This reverts commit aef5001070258394e90387b9b94d36f06da42516.

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ Ext.define('PopupFormController', {
   handleSubmit: function(params, success, failure) {
     Ext.Ajax.request({
       url: Configs.baseUrl + 'Books',
-      methods: 'PUT',
+      method: 'PUT',
       params: params,
 
       success: function(response, opts) {
